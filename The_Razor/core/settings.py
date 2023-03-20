0d4c4fd8125fb062e6768bf43787100a3b4c537b
geissonb <geissonbarbacena@gmail.com>
2023-03-20 15:40:06 -0300

    modified:   The_Razor/core/settings.py 	modified:   The_Razor/requirements.txt

--- The_Razor/core/settings.py
+++ The_Razor/core/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'The_Razor',
+        'USER': 'root',
+        'PASSOWORD': 'newpass',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -105,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
